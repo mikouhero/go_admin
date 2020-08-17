852fad21b17a3d5dbef309069a1305848b34af74
mikouhero <290468335@qq.com>
2020-08-17 22:13:51 +0800

    jwt

--- Server/middleware/jwt.go
+++ Server/middleware/jwt.go
@@ -2,11 +2,14 @@ package middleware
 
 import (
 	"errors"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"go_admin/Server/global"
 	"go_admin/Server/global/response"
 	"go_admin/Server/model"
+	"go_admin/Server/model/request"
 	"go_admin/Server/service"
+	"time"
 )
 
 func JWTAuth() gin.HandlerFunc {
@@ -49,7 +52,61 @@ func NewJWT() *JWT {
 }
 
 // 创建token
+func (j *JWT) CreateToken(claims request.CustomClaims) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+
+	return token.SignedString(j.SigningKey)
+}
 
 // 删除token
+func (j *JWT) ParseToken(tokenString string) (*request.CustomClaims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &request.CustomClaims{}, func(token *jwt.Token) (i interface{}, err error) {
+		return j.SigningKey, nil
+	})
+
+	if err != nil {
+		if ve, ok := err.(*jwt.ValidationError); ok {
+			if ve.Errors&jwt.ValidationErrorMalformed != 0 {
+				return nil, TokenMalformed
+			} else if ve.Errors&jwt.ValidationErrorExpired != 0 {
+				return nil, TokenExpired
+			} else if ve.Errors&jwt.ValidationErrorNotValidYet != 0 {
+				return nil, TokenNotValidYet
+			} else {
+				return nil, TokenInvalid
+			}
+		}
+	}
+
+	if token != nil {
+		if claims, ok := token.Claims.(*request.CustomClaims); ok && token.Valid {
+			return claims, nil
+		}
+		return nil, TokenInvalid
+	} else {
+		return nil, TokenInvalid
+	}
+
+}
 
 // 更新token
+func (j *JWT) RefreshToken(tokenString string) (string, error) {
+	jwt.TimeFunc = func() time.Time {
+		return time.Unix(0, 0)
+	}
+
+	token, err := jwt.ParseWithClaims(tokenString, &request.CustomClaims{}, func(token *jwt.Token) (i interface{}, err error) {
+		return j.SigningKey, nil
+	})
+
+	if err != nil {
+		return "", nil
+	}
+
+	if claims, ok := token.Claims.(*request.CustomClaims); ok && token.Valid {
+		jwt.TimeFunc = time.Now
+		claims.StandardClaims.ExpiresAt = time.Now().Add(1 * time.Hour).Unix()
+		return j.CreateToken(*claims)
+	}
+	return "", TokenInvalid
+}
