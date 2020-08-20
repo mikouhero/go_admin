c7416bfce6d0d25aaaec884b482182ea1c6e5b8b
mikouhero <290468335@qq.com>
2020-08-20 22:59:08 +0800

    角色规划

---
+++ Server/api/v1/sys_authority.go
@@ -0,0 +1,28 @@
+package v1
+
+import "github.com/gin-gonic/gin"
+
+func CreateAuthority(c *gin.Context) {
+
+}
+
+func CopyAuthority(c *gin.Context)  {
+	
+}
+
+func DeleteAuthority(c *gin.Context)  {
+	
+}
+
+func UpdateAuthority(c *gin.Context)  {
+	
+}
+
+func GetAuthorityList(c *gin.Context)  {
+	
+}
+
+// 设置资源权限
+func SetDataAuthority(c *gin.Context)  {
+	
+}
---
+++ Server/service/sys_authority.go
@@ -0,0 +1,37 @@
+package service
+
+func CreateAuthority()  {
+	
+}
+
+func CopyAuthority()  {
+	
+}
+
+func UpdateAuthority()  {
+	
+}
+
+func DeleteAuthority()  {
+	
+}
+
+func GetAuthorityInfoList()  {
+	
+}
+
+func GetAuthorityInfo()  {
+	
+}
+
+func SetDataAuthority()  {
+	
+}
+
+func SetMenuAuthority()  {
+	
+}
+
+func FindChildrenAuthority()  {
+	
+}
