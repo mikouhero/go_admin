04f95d9123468913c6f52c9941ffa566be49f8c1
mikouhero <290468335@qq.com>
2020-09-01 22:02:36 +0800

    模拟菜单

--- Server/service/sys_base_menu.go
+++ Server/service/sys_base_menu.go
@@ -6,6 +6,18 @@ import (
 	"go_admin/Server/model"
 )
 
+// 添加菜单
+func AddBaseMenu(menu model.SysBaseMenu) error {
+	err := global.GVA_DB.Where("name = ? ", menu.Name).Find(&model.SysBaseMenu{}).Error
+	if err != nil {
+		menu.Hidden = "1"
+		err = global.GVA_DB.Create(&menu).Error
+	} else {
+		return errors.New("存在相同的菜单名称(name)，请修改name")
+	}
+	return err
+}
+
 func DeleteBaseMenu(id float64) (err error) {
 
 	count := 0
--- Server/service/sys_menu.go
+++ Server/service/sys_menu.go
@@ -1,55 +1,92 @@
 package service
 
 import (
-	"errors"
 	"go_admin/Server/global"
 	"go_admin/Server/model"
+	"strconv"
 )
 
-func GetMenuTreeMap() {
-
+func getMenuTreeMap(authorityId string) (err error, treeMap map[string][]model.SysMenu) {
+	var allMenus []model.SysMenu
+	treeMap = make(map[string][]model.SysMenu)
+	err = global.GVA_DB.
+		Unscoped().
+		Where("authority_id = ?", authorityId).
+		Order("sort", true).
+		//Preload("Parameters").
+		Find(&allMenus).
+		Error
+	for _, v := range allMenus {
+		treeMap[v.ParentId] = append(treeMap[v.ParentId], v)
+	}
+	return err, treeMap
 }
 
-func GetMenuTree() {
-
+func GetMenuTree(authorityId string) (err error, menus []model.SysMenu) {
+	err, menuTree := getMenuTreeMap(authorityId)
+	menus = menuTree["0"]
+	for i := 0; i < len(menus); i++ {
+		err = getChildrenList(&menus[i], menuTree)
+	}
+	return err, menus
 }
 
-func getChildrenList() {
-
-}
-
-func GetInfoList() {
-
-}
-
-func GetBaseChildrenList() {
-
-}
-
-// 添加菜单
-func AddBaseMenu(menu model.SysBaseMenu) error {
-	err := global.GVA_DB.Where("name = ? ", menu.Name).Find(&model.SysBaseMenu{}).Error
-	if err != nil {
-		menu.Hidden = "1"
-		err = global.GVA_DB.Create(&menu).Error
-	} else {
-		return errors.New("存在相同的菜单名称(name)，请修改name")
+func getChildrenList(menu *model.SysMenu, treeMap map[string][]model.SysMenu) (err error) {
+	menu.Children = treeMap[menu.MenuId]
+	for i := 0; i < len(menu.Children); i++ {
+		err = getChildrenList(&menu.Children[i], treeMap)
 	}
 	return err
 }
 
-func GetBaseMenuTreeMap() {
-
+func GetInfoList() (err error, list interface{}, total int) {
+	var menuList []model.SysBaseMenu
+	err, treeMap := getBaseMenuTreeMap()
+	menuList = treeMap["0"]
+	for i := 0; i < len(menuList); i++ {
+		err = getBaseChildrenList(&menuList[i], treeMap)
+	}
+	return err, menuList, total
 }
 
-func GetBaseMenuTree() {
-
+func getBaseChildrenList(menu *model.SysBaseMenu, treeMap map[string][]model.SysBaseMenu) (err error) {
+	menu.Children = treeMap[strconv.Itoa(int(menu.ID))]
+	for i := 0; i < len(menu.Children); i++ {
+		err = getBaseChildrenList(&menu.Children[i], treeMap)
+	}
+	return err
 }
 
-func AddMenuAuthority() {
-
+func getBaseMenuTreeMap() (err error, treeMap map[string][]model.SysBaseMenu) {
+	var allMenus []model.SysBaseMenu
+	treeMap = make(map[string][]model.SysBaseMenu)
+	err = global.GVA_DB.Order("sort", true).Preload("Parameters").Find(&allMenus).Error
+	for _, v := range allMenus {
+		treeMap[v.ParentId] = append(treeMap[v.ParentId], v)
+	}
+	return err, treeMap
 }
 
-func GetMenuAuthority() {
+func GetBaseMenuTree() (err error, menus []model.SysBaseMenu) {
+	err, treeMap := getBaseMenuTreeMap()
+	menus = treeMap["0"]
+	for i := 0; i < len(menus); i++ {
+		err = getBaseChildrenList(&menus[i], treeMap)
+	}
+	return err, menus
+}
 
+func AddMenuAuthority(menus []model.SysBaseMenu, authorityId string) (err error) {
+	var auth model.SysAuthority
+	auth.AuthorityId = authorityId
+	auth.SysBaseMenus = menus
+	err = SetMenuAuthority(&auth)
+	return err
+}
+
+func GetMenuAuthority(authorityId string) (err error, menus []model.SysMenu) {
+	//sql := "SELECT authority_menu.keep_alive,authority_menu.default_menu,authority_menu.created_at,authority_menu.updated_at,authority_menu.deleted_at,authority_menu.menu_level,authority_menu.parent_id,authority_menu.path,authority_menu.`name`,authority_menu.hidden,authority_menu.component,authority_menu.title,authority_menu.icon,authority_menu.sort,authority_menu.menu_id,authority_menu.authority_id FROM authority_menu WHERE authority_menu.authority_id = ? ORDER BY authority_menu.sort ASC"
+	err = global.GVA_DB.Where("authority_id = ? ", authorityId).Order("sort", true).Find(&menus).Error
+	//err = global.GVA_DB.Raw(sql, authorityId).Scan(&menus).Error
+	return err, menus
 }
